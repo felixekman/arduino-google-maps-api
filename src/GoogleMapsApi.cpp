c5d96caebf622cf50ae5e574f74ea8e068998d71
Felix <me@felixekman.com>
2017-03-05 17:52:41 +0100

    Added transit mode

--- src/GoogleMapsApi.cpp
+++ src/GoogleMapsApi.cpp
@@ -80,13 +80,16 @@ String GoogleMapsApi::sendGetToGoogleMaps(String command) {
   return body;
 }
 
-String GoogleMapsApi::distanceMatrix(String origin, String destination, String departureTime, String trafficModel) {
+String GoogleMapsApi::distanceMatrix(String origin, String destination, String departureTime, String trafficModel, String transitMode) {
   String command="https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + origin + "&destinations=" + destination; //If you can't find it(for example if you have a custom url) look here: https://www.google-maps.com/account_advanced
   if (departureTime != "") {
     command = command + "&departure_time=" + departureTime;
   }
   if (trafficModel != "") {
     command = command + "&traffic_model=" + trafficModel;
+  }
+  if (transitMode != "") {
+    command = command + "&transit_mode=" + transitMode;
   }
 
   command = command + "&key=" + _apiKey;
